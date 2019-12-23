13915b6997990dd2acc4c34d7b539fa007d184b0
Chris Dryden <chrisdryden2@gmail.com>
2019-12-23 17:00:29 +0000

    types started, altered default html

---
+++ noodles/types.js
@@ -0,0 +1,35 @@
+const name = 'Gideon';
+const middle = 'Jole';
+const last = 'Brimleaf';
+
+const sentence = "she's so 'cool'";
+const sentence2 = `she's so "cool"`;
+
+const multiline = `Ohhh
+Yea
+
+Use
+
+The Force Frodeo!`;
+
+const interpol = `Hello, my name is ${name} nice to meet you
+
+I am ${12*3} years old`
+
+const html = `
+  <div>
+    <h2>${name}</h2>
+    <p>${interpol}</p>
+  </div>
+`;
+
+document.body.innerHTML = html;
+
+const smarties = 20;
+const kids = 3;
+const dadGets = smarties % kids
+
+const eachKidGets = Math.floor(smarties/kids);
+
+console.log(`each kid gets ${eachKidGets} smarties
+dad gets ${dadGets} smarties`)

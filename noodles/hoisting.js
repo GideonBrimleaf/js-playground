f45482865dc1d258450e4d6c427d5cee2d197f42
Chris Dryden <chrisdryden2@gmail.com>
2019-12-30 16:21:36 +0000

    just got my mind blown by closures

---
+++ noodles/hoisting.js
@@ -0,0 +1,13 @@
+console.log(age)
+var age = 10
+
+saySomething()
+
+function saySomething() {
+  console.log('filthy junk traders')
+  console.log(add(10,2))
+}
+
+function add(a,b) {
+  return a+b
+}
